ec921a7868c5175930a2a359fd908588e0efdef3
Alex <Alexeydustfg@Gmail.com>
2025-07-18 22:52:45 +0300

    Тесты для продуктов, (UNIT OF WORK)

---
+++ Gedonist.Tests/ProductControllerTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gedonist.Core.Models;
+using Gedonist.Core.Interfaces;
+using Moq;
+using Gedonist.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Gedonist.Core.BindingModels;
+
+namespace Gedonist.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public async Task GetAll_Collection_OKCollection()
+        {
+            //Arrange
+            List<Product> products = new List<Product>();
+            var mock = new Mock<IProductsService>();
+            mock.Setup(m => m.GetAll()).ReturnsAsync(products);
+            ProductsController controller = new ProductsController(mock.Object);
+            //Action
+            var result = await controller.GetAll();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetAll_Null_NotFound()
+        {
+            //Arrange
+            var mock = new Mock<IProductsService>();
+            mock.Setup(m => m.GetAll()).Returns(Task.FromResult<List<Product>?>(null));
+            ProductsController controller = new ProductsController(mock.Object);
+            //Action
+            var result = await controller.GetAll();
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+
+        [Fact]
+        public async Task Delete_int_OK()
+        {
+            //Arrange
+            var mock = new Mock<IProductsService>();
+            mock.Setup(m => m.Delete(1)).ReturnsAsync(true);
+            ProductsController controller = new ProductsController(mock.Object);
+            //Action
+            var result = await controller.Delete(1);
+
+            //Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_noncorrectint_NotFound()
+        {
+            //Arrange
+            var mock = new Mock<IProductsService>();
+            mock.Setup(m => m.Delete(-1)).ReturnsAsync(false);
+            ProductsController controller = new ProductsController(mock.Object);
+            //Action
+            var result = await controller.Delete(-1);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Create_newmodel_Created()
+        {
+            //Arrange
+            BindingProduct product = new BindingProduct();
+            var mock = new Mock<IProductsService>();
+            mock.Setup(x => x.Create(product)).ReturnsAsync(true);
+            ProductsController controller = new ProductsController(mock.Object);
+            //Action
+            var result = await controller.Create(product);
+
+            //Assert
+            Assert.IsType<CreatedResult>(result);
+        }
+
+        [Fact]
+        public async Task Create_oldmodel_NotFound()
+        {
+            //Arrange
+            BindingProduct product = new BindingProduct();
+            var mock = new Mock<IProductsService>();
+            mock.Setup(x => x.Create(product)).ReturnsAsync(false);
+            ProductsController controller = new ProductsController(mock.Object);
+            //Action
+            var result = await controller.Create(product);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task GetById_int_Okmodel()
+        {
+            //Arrange
+            var mock = new Mock<IProductsService>();
+            mock.Setup(x => x.GetById(1)).ReturnsAsync(new Product { Name = "PP" });
+            ProductsController controller = new ProductsController(mock.Object);
+            //Action
+            var result = await controller.GetById(1);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task GetById_noncorrectint_NotFound()
+        {
+            //Arrange
+            var mock = new Mock<IProductsService>();
+            mock.Setup(x => x.GetById(-2)).Returns(Task.FromResult<Product?>(null));
+            ProductsController controller = new ProductsController(mock.Object);
+            //Action
+            var result = await controller.GetById(-2);
+
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+
+
+    }
+}
